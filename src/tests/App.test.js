0493705f0c7ff7d07aedd802448af7203ecade8b
Matheus <matheusjesus885@outlook.com>
2023-01-25 19:31:26 -0300

    task4 ok

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,40 +3,83 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import Header from '../components/Header';
+import App from '../App';
+import { renderWithRouter } from '../helpers/renderWith';
 
-test('Verifica se existe um title e uma image de profile', () => {
-  render(
-    <BrowserRouter>
-      <Header />
-    </BrowserRouter>,
-  );
+const email = 'trybeteste@hotmail.com';
+const senha = '12345678';
 
-  const titleProfile = screen.getByTestId('page-title');
-  const imgProfile = screen.getByRole('img', {
-    name: /profileicon/i,
+describe('Componente Header', () => {
+  test('Verifica se existe um title e uma image de profile', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
+
+    const titleProfile = screen.getByTestId('page-title');
+    const imgProfile = screen.getByRole('img', {
+      name: /profileicon/i,
+    });
+
+    expect(titleProfile).toBeInTheDocument();
+    expect(imgProfile).toBeInTheDocument();
   });
 
-  expect(titleProfile).toBeInTheDocument();
-  expect(imgProfile).toBeInTheDocument();
+  test('Verifica se existe o icon Search e se aparece/desaparece o inputSearch ao clicar', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>,
+    );
+
+    const imgSearch = screen.getByRole('img', {
+      name: /searchicon/i,
+    });
+
+    userEvent.click(imgSearch);
+    const inputSearch = screen.getByRole('textbox');
+    expect(inputSearch).toBeInTheDocument();
+
+    userEvent.click(imgSearch);
+    expect(inputSearch).not.toBeInTheDocument();
+
+    expect(imgSearch).toBeInTheDocument();
+  });
 });
 
-test('Verifica se existe o icon Search e se aparece/desaparece o inputSearch ao clicar', () => {
-  render(
-    <BrowserRouter>
-      <Header />
-    </BrowserRouter>,
-  );
+describe('Componente Login', () => {
+  test('Verifica se existem os inputs "email", "senha" e o botão "entrar"', () => {
+    renderWithRouter(<App />);
 
-  const imgSearch = screen.getByRole('img', {
-    name: /searchicon/i,
+    const inputEmail = screen.getByRole('textbox');
+    const inputSenha = screen.getByPlaceholderText(/senha:/i);
+    const btnEntrar = screen.getByRole('button', {
+      name: /entrar/i,
+    });
+
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputSenha).toBeInTheDocument();
+    expect(btnEntrar).toBeInTheDocument();
   });
 
-  userEvent.click(imgSearch);
-  const inputSearch = screen.getByRole('textbox');
-  expect(inputSearch).toBeInTheDocument();
+  test('Verifica se ao digitar email e senha o botão entrar é habilitado', () => {
+    renderWithRouter(<App />);
 
-  userEvent.click(imgSearch);
-  expect(inputSearch).not.toBeInTheDocument();
+    const inputEmail = screen.getByRole('textbox');
+    const inputSenha = screen.getByPlaceholderText(/senha:/i);
+    const btnEntrar = screen.getByRole('button', {
+      name: /entrar/i,
+    });
 
-  expect(imgSearch).toBeInTheDocument();
+    userEvent.type(inputEmail, email);
+    userEvent.type(inputSenha, senha);
+
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputSenha).toBeInTheDocument();
+    expect(btnEntrar).toBeInTheDocument();
+    expect(btnEntrar).toBeEnabled();
+
+    userEvent.click(btnEntrar);
+  });
 });

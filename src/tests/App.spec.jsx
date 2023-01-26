c9f8ad77a184b873042b46525ef8bc00346f68fc
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-01-26 17:39:03 -0300

    testes footer

--- src/tests/App.spec.jsx
+++ src/tests/App.spec.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import Header from '../components/Header';
 import App from '../App';
 import { renderWithRouter } from '../helpers/renderWith';
+import Footer from '../components/Footer';
 
 const email = 'trybeteste@hotmail.com';
 const senha = '12345678';
@@ -84,5 +85,48 @@ describe('Componente Login', () => {
     expect(btnEntrar).toBeEnabled();
 
     userEvent.click(btnEntrar);
+  });
+  describe('Componente Footer', () => {
+    test('Se os elementos do footer são renderizados na página', () => {
+      render(
+        <BrowserRouter>
+          <Footer />
+        </BrowserRouter>,
+      );
+
+      const mealImg = screen.getByRole('img', {
+        name: /meal icon/i,
+      });
+      const drinkImg = screen.getByRole('img', {
+        name: /drink icon/i,
+      });
+
+      expect(mealImg).toBeInTheDocument();
+      expect(drinkImg).toBeInTheDocument();
+    });
+    test('Se ao clicar no button drinks, a página é redirecionada', () => {
+      const { history } = renderWithRouter(<App />);
+      act(() => { history.push('/meals'); });
+
+      const btnDrink = screen.getByRole('button', {
+        name: /drink icon/i,
+      });
+      expect(btnDrink).toBeInTheDocument();
+      userEvent.click(btnDrink);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/drinks');
+    });
+    test('Se ao clicar no button meals, a página é redirecionada', () => {
+      const { history } = renderWithRouter(<App />);
+      act(() => { history.push('/drinks'); });
+
+      const btnMeal = screen.getByRole('button', {
+        name: /meal icon/i,
+      });
+      expect(btnMeal).toBeInTheDocument();
+      userEvent.click(btnMeal);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/meals');
+    });
   });
 });

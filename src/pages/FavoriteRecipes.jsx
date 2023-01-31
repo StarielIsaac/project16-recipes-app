032ad51075d28aa92d6f6dececff865abbb543ad
Bernardo Marques <bernardomp.dev@gmail.com>
2023-01-31 16:18:11 -0300

    requirement 50 ready to receive data from local storage

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -2,10 +2,64 @@ import Header from '../components/Header';
 
 function FavoriteRecipes() {
   return (
-    <>
+    <div>
       <Header title="Favorite Recipes" />
-      <h1>FavoriteRecipes</h1>
-    </>
+      <forms>
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+        <br />
+        <br />
+        {/* Fazer map dos elementos contidos em localStorage.getItem('favoriteRecipes') */}
+        <img
+          src="provisorio"
+          alt="provisorio"
+          // data-testid={ `${index}-horizontal-image"` }
+        />
+        <br />
+        <br />
+        <p
+          name="HorizontalTopText"
+          // data-testid={ `${index}-horizontal-top-text` }
+        >
+          Categoria: Provisorio
+        </p>
+        <br />
+        <p
+          name="HorizontalName"
+          // data-testid={ `${index}-horizontal-name` }
+        >
+          Nome da Receita: Provisorio
+        </p>
+        <button
+          type="button"
+          // data-testid={ `${index}-horizontal-share-btn` }
+        >
+          Compartilhar a Receita
+        </button>
+        <button
+          type="button"
+          // data-testid={ `${index}-horizontal-favorite-btn` }
+        >
+          favoritar a receita
+        </button>
+      </forms>
+    </div>
   );
 }
 

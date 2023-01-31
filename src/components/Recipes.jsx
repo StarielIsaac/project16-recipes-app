b34788ea392968a35c6933b95ada2d1287735942
Matheus <matheusjesus885@outlook.com>
2023-01-31 16:50:26 -0300

    task22 ok

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -3,15 +3,18 @@ import { withRouter } from 'react-router-dom';
 import RecipesContext from '../context/RecipesContext';
 import { fetchDefaulCategoriesMeals,
   fetchDefaultCategoriesDrinks } from '../services/ApiCategories';
+import { fetchByCategoryDrinks,
+  fetchByCategoryMeals } from '../services/ApiFilterbyCategories';
 import {
   fetchDefaultMeals, fetchDefaultDrinks,
 } from '../services/ApiMealsOrDrinksDefaul';
 
 function Recipes({ history }) {
-  const { recipes } = useContext(RecipesContext);
+  const { recipes, setRecipes } = useContext(RecipesContext);
   const [renderRecipes, setRenderRecipes] = useState(null);
   const [categories, setCategories] = useState([]);
   const [categoresFilter, setCategoriesFilter] = useState([]);
+  const [ExistFilter, setExisfilter] = useState('');
 
   const maxRecipes = 12;
   const maxCategories = 5;
@@ -73,7 +76,31 @@ function Recipes({ history }) {
     setCategories(categoriesDefault);
   };
 
+  const fetchByCategory = async ({ target }) => {
+    console.log('exis', ExistFilter, 'targt', target.value);
+    if (ExistFilter === target.value) {
+      setExisfilter('');
+      return setRecipes(null);
+    }
+    let byCategories;
+    if (history.location.pathname === '/meals') {
+      byCategories = await fetchByCategoryMeals(target.value);
+    }
+    if (history.location.pathname === '/drinks') {
+      byCategories = await fetchByCategoryDrinks(target.value);
+    }
+    setRecipes(byCategories);
+    setExisfilter(target.value);
+  };
+
+  const clickAllFilter = () => {
+    setRecipes(null);
+  };
+
   useEffect(() => {
+    if (recipes === null) {
+      checkDefaultFetch();
+    }
     filterRecipesOrDefaultRecipes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [recipes]);
@@ -100,11 +127,21 @@ function Recipes({ history }) {
           <button
             key={ index }
             data-testid={ `${category.strCategory}-category-filter` }
+            value={ category.strCategory }
+            onClick={ fetchByCategory }
           >
             {category.strCategory}
           </button>
         ))}
       </nav>
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ clickAllFilter }
+      >
+        ALL
+
+      </button>
       <div>
         {renderRecipes && renderRecipes.map((recipe, index) => (
           <div data-testid={ `${index}-recipe-card` } key={ index }>
